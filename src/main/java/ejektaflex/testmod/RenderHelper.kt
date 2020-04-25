7f96c50abc7b32b8e28cc2c506c8b488dc89e852
Ejektaflex <dootydooter@gmail.com>
2020-04-25 08:01:37 -0500

    Re-implement custom position rendering on top of blocks

--- src/main/java/ejektaflex/testmod/RenderHelper.kt
+++ src/main/java/ejektaflex/testmod/RenderHelper.kt
@@ -1,7 +1,29 @@
 package ejektaflex.testmod
 
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.VertexFormat
+import net.minecraft.client.render.VertexFormats
+import org.lwjgl.opengl.GL11
+import java.util.*
+
 object RenderHelper {
 
-
+    @Suppress("INACCESSIBLE_TYPE")
+    class MyRenderLayer(name: String?, format: VertexFormat?, p_i225992_3_: Int, p_i225992_4_: Int, p_i225992_5_: Boolean, p_i225992_6_: Boolean, runnablePre: Runnable?, runnablePost: Runnable?) : RenderLayer(name, format, p_i225992_3_, p_i225992_4_, p_i225992_5_, p_i225992_6_, runnablePre, runnablePost) {
+        companion object {
+            //private val THICK_LINES: LineState = LineState(OptionalDouble.of(3.0))
+            val OVERLAY_LINES: RenderLayer = of("overlay_lines",
+                    VertexFormats.POSITION_COLOR, GL11.GL_LINES, 256,
+                    MultiPhaseParameters.builder()
+                            .lineWidth(LineWidth(OptionalDouble.of(2.0)))
+                            .transparency(TRANSLUCENT_TRANSPARENCY)
+                            .texture(NO_TEXTURE)
+                            .depthTest(DepthTest(519))
+                            .cull(Cull(false))
+                            .lightmap(Lightmap(false))
+                            .writeMaskState(WriteMaskState(true, false))
+                            .build(false))
+        }
+    }
 
 }
--- src/main/java/ejektaflex/testmod/mixin/WorldRendererMixin.kt
+++ src/main/java/ejektaflex/testmod/mixin/WorldRendererMixin.kt
@@ -1,15 +1,20 @@
 package ejektaflex.testmod.mixin
 
+import com.mojang.blaze3d.systems.RenderSystem
+import ejektaflex.testmod.RenderHelper
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.*
 import net.minecraft.client.util.math.Matrix4f
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
+import org.lwjgl.opengl.GL11
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.gen.Accessor
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import java.util.*
 
 
 @Mixin(WorldRenderer::class)
@@ -21,19 +26,43 @@ abstract class WorldRendererMixin {
     //@Shadow
     //private val capturedFrustum: Frustum? = null
 
+    private val mc = MinecraftClient.getInstance()
+
+    private fun makeBox(view: Vec3d, pos: Vec3d, size: Vec3d): Box {
+        return Box(
+                pos.x, pos.y, pos.z,
+                pos.x + size.x, pos.y + size.y, pos.z + size.z
+        ).offset(pos.x - view.x, pos.y - view.y, pos.z - view.z)
+    }
+
+
+
     @Inject(method = ["render"], at = [At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;checkEmpty(Lnet/minecraft/client/util/math/MatrixStack;)V", ordinal = 0)])
     private fun render(
             matrices: MatrixStack, tickDelta: Float, limitTime: Long, renderBlockOutline: Boolean,
             camera: Camera, gameRenderer: GameRenderer, lightmapTextureManager: LightmapTextureManager,
             projection: Matrix4f, ci: CallbackInfo
     ) {
-        val immediate = MinecraftClient.getInstance().gameRenderer
+
+
+
+
+        RenderSystem.pushMatrix()
+
         WorldRenderer.drawBox(
                 matrices,
-                getBufferBuilders().entityVertexConsumers.getBuffer(RenderLayer.getLines()),
-                Box(0.0, 0.0, 0.0, 1.0, 1.0, 1.0),
+                getBufferBuilders().entityVertexConsumers.getBuffer(RenderHelper.MyRenderLayer.OVERLAY_LINES),
+                makeBox(
+                        camera.pos,
+                        Vec3d(3.0, 3.0, 3.0),
+                        Vec3d(2.0, 2.0, 2.0)
+                ),
                 1f, 1f, 1f, 1f
         )
+        //RenderSystem.enableDepthTest()
+
+        RenderSystem.popMatrix()
+
     }
 
 }
