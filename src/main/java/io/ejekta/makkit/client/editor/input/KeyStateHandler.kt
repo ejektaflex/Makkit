3c107d9fa8a417c6076ffa0d062315b822711c25
Ejektaflex <dootydooter@gmail.com>
2020-05-22 03:43:40 -0500

    Hacky workaround for multiple keybinds with the same base keycode

--- src/main/java/io/ejekta/makkit/client/editor/input/KeyStateHandler.kt
+++ src/main/java/io/ejekta/makkit/client/editor/input/KeyStateHandler.kt
@@ -1,13 +1,18 @@
 package io.ejekta.makkit.client.editor.input
 
+import io.ejekta.makkit.client.MakkitClient
 import io.ejekta.makkit.client.editor.IEditor
 import io.ejekta.makkit.common.MakkitCommon
+import me.shedaniel.clothconfig2.api.Modifier
 import me.shedaniel.clothconfig2.api.ModifierKeyCode
 import net.minecraft.client.MinecraftClient
 import net.minecraft.text.MutableText
 import net.minecraft.text.TranslatableText
+import kotlin.math.abs
 
 class KeyStateHandler(val id: String, var binding: ModifierKeyCode) : IEditor {
+
+    var lastReleased = -1L
 
     val isScreenOpen: Boolean
         get() = MinecraftClient.getInstance().currentScreen != null
@@ -29,6 +34,16 @@ class KeyStateHandler(val id: String, var binding: ModifierKeyCode) : IEditor {
         onKeyUp = func
     }
 
+    // Picks up any other keys that got released <500ms ago
+    fun recentlyMatchedOtherKeys(): Boolean {
+        val matchedKeys = MakkitClient.config.keys.filter {
+            it.binding.keyCode.equals(binding.keyCode)
+        }
+        return matchedKeys.any {
+            (it.isDown && it != this) || abs(System.currentTimeMillis() - it.lastReleased) < 200L
+        }
+    }
+
     private var onKeyDown = {}
 
     private var onKeyUp = {}
@@ -36,12 +51,21 @@ class KeyStateHandler(val id: String, var binding: ModifierKeyCode) : IEditor {
     override fun update() {
         // Try to start dragging
         if (!isScreenOpen && !isDown && (binding.matchesCurrentKey() || binding.matchesCurrentMouse())) {
-            isDown = true
-            onKeyDown()
+            // Hacky way to prevent Z getting hit after, say, CTRL+Z then letting go of Z
+            if (!recentlyMatchedOtherKeys()) {
+                isDown = true
+                onKeyDown()
+            }
         }
 
+        val fakeKey = ModifierKeyCode.of(binding.keyCode, Modifier.none())
+
         // Try to stop dragging
-        if (isDown && (!binding.matchesCurrentKey() && !binding.matchesCurrentMouse())) {
+        if (isDown && (!binding.matchesCurrentKey()
+                        && !fakeKey.matchesCurrentKey()
+                        && !binding.matchesCurrentMouse())
+        ) {
+            lastReleased = System.currentTimeMillis()
             onKeyUp()
             isDown = false
         }
