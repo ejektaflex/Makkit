b88912b4581fc59c8be157f014b60953a10e60cb
Ejektaflex <dootydooter@gmail.com>
2020-06-23 17:37:11 -0500

    Official port to 1.16

--- src/main/java/io/ejekta/makkit/common/network/pakkit/ServerBoundPakkit.kt
+++ src/main/java/io/ejekta/makkit/common/network/pakkit/ServerBoundPakkit.kt
@@ -6,8 +6,8 @@ interface ServerBoundPakkit : Pakkit {
     fun sendToServer() {
 
         ClientSidePacketRegistry.INSTANCE.sendToServer(getId(), write()).also {
-            println("Sending packet to server: '${getId()}'. Is server set to receive the packet?: " +
-             "${ClientSidePacketRegistry.INSTANCE.canServerReceive(getId())}")
+            //println("Sending packet to server: '${getId()}'. Is server set to receive the packet?: " +
+            // "${ClientSidePacketRegistry.INSTANCE.canServerReceive(getId())}")
         }
     }
 }
